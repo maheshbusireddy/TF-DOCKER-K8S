a4e18145547ed29e812869257caba7d22234881c
maheshbusireddy <busireddymahesh@gmail.com>
2023-06-22 19:58:18 +0530

    terraform module

--- TF-DOCKER-K8S/ec2.tf
+++ TF-DOCKER-K8S/ec2.tf
@@ -3,7 +3,4 @@ resource "aws_instance" "your-wish" {
     instance_type = "t2.micro"
 }
 
-/* resource "aws_instance" "your-wish" {
-    ami = "ami-012b9156f755804f5"
-    instance_type = "t2.micro"
-} */
+
